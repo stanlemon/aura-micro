0beff212045d70aadaa40552679a7eff26105028
Stan Lemon <stosh1985@gmail.com>
2012-12-15 09:41:51 -0500

    Stubbed out testing stuff, see issue #4

---
+++ tests/Aura/Micro/MicroTest.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * @package Aura.Micro
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace Aura\Micro;
+
+/**
+ * 
+ * @package Aura.Micro
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ * 
+ * 
+ */
+class MicroTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAdd() {
+        $app = new \Aura\Micro\Micro();
+        // @todo - write some tests
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+$autoload = include dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+$autoload->add('Aura\Micro', __DIR__ . DIRECTORY_SEPARATOR . 'Aura' . DIRECTORY_SEPARATOR . 'Micro');
